d6cac1787a0e3b43ac369d3ec21a571b5f2d9070
Damira Shartdinova <Damira.damirovna@bk.ru>
2024-02-24 17:33:03 +0300

    Добавлеы rr3

--- Task1/Program.cs
+++ Task1/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+//Console.writeline(Hello world);
+
+
+int value = 100;
+
+Console.writeline(value);
+
+double d = 5.55;
+char c = '!';
+bool b = true;
+Console.writeline(double);
+Console.writeline(char);
+Console.writeline(bool);
