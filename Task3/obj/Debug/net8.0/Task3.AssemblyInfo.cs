302e0fb431ed29d062391fdbc3318c6f1383320f
Damira Shartdinova <Damira.damirovna@bk.ru>
2024-02-24 18:18:33 +0300

    Пишем псевдокод1

--- Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
+++ Task3/obj/Debug/net8.0/Task3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eb70dc4ed6d048d84bbd9eae54ec226beebae274")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59907424826b50f73b782f03355d6161fb24dafd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
